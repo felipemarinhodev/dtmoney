ff6dccdbe79c867bb2780fad9ced8bcb7ea1096d
Felipe Marinho <felipmarinho@gmail.com>
2021-07-22 17:09:47 -0300

    remove styled-component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,7 @@
-import styled from 'styled-components';
-
-const Title = styled.h1`
-    font-size: 64px;
-    color: #8257e6;
-`;
-
 export function App() {
   return (
     <div className="App">
-      <Title>Hello DT Money</Title>
+      <h1>Hello DT Money</h1>
     </div>
   );
 }
