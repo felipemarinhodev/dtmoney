ea9f0182c081ca5ac678bfd5b69d28adf368f46e
Felipe Marinho <felipmarinho@gmail.com>
2021-07-23 12:19:00 -0300

    set info to miragejs return

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createServer } from 'miragejs'
 import { App } from './App';
+
+createServer({
+  routes() {
+    this.namespace = "api";
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Transition 1',
+          amount: 1020,
+          date: '2016-01-01',
+        }
+      ]
+    })
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
