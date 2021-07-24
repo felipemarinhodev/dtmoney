9a2adb2d7b3a1d84c98ed56f648e8e19011fbf0b
Felipe Marinho <felipmarinho@gmail.com>
2021-07-24 20:35:47 -0300

    change for use hook

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,5 +1,4 @@
-import { useContext } from 'react';
-import { TransactionsContext } from '../../TransactionsContext';
+import { useTransactions } from '../../hooks/useTransactions';
 
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
@@ -7,8 +6,10 @@ import totalImg from '../../assets/total.svg';
 
 import { Container } from './styles';
 
+
+
 export function Summary() {
-  const { transactions } = useContext(TransactionsContext);
+  const { transactions } = useTransactions();
 
   const summary = transactions.reduce(
     (acc, transaction) => {
