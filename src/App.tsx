4ef0f8767f9a924739a1d589999b47cc40398f9b
Felipe Marinho <felipmarinho@gmail.com>
2021-07-24 20:35:13 -0300

    update path of component

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import Modal from 'react-modal';
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
 import { NewTransactionModal } from './components/NewTransactionModal';
+import { TransactionsProvider } from './hooks/useTransactions';
 import { GlobalStyles } from './styles/global';
-import { TransactionsProvider } from './TransactionsContext';
 
 Modal.setAppElement('#root');
 
